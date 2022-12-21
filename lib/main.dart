36557270ca11a8c471d1245754cba1cac5d285bb
Shirinboyev <gayratshirinboyev6707@gmail.com>
2022-12-21 11:12:04 +0500

    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
   State<MyApp> createState() => _MyAppState();
 }
-int scale = 75;
+
+double scale = 00.000;
 bool val = false;
 bool cal = false;
+bool den = false;
+bool bal = false;
 bool dal = false;
+bool sal = false;
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
@@ -23,7 +30,7 @@ class _MyAppState extends State<MyApp> {
                 child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset('images/pizza.png'),
+                    Text('Foods',style: TextStyle(fontSize: 35,color: Colors.blue),),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -33,40 +40,24 @@ class _MyAppState extends State<MyApp> {
                         setState(() {
                           val = !val;
                           if (val) {
-                            scale += 5;
+                            scale += 65;
                           } else
-                            scale -= 5;
+                            scale -= 65;
                         });
                       },
-                      side: BorderSide(color: Colors.black),
                       activeColor: Colors.blue,
-                      // autofocus: true,
                     ),
-                    Text('Extra Chesse '),
+                    Text(
+                      'Pizza chicken      ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                      ),
+                    ),
+                Image.asset('images/pizza.png',scale: 2.5,),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Checkbox(
-                      value: cal,
-                      onChanged: (value) {
-                        setState(() {
-                          cal = !cal;
-                          if (cal) {
-                            scale += 5;
-                          } else
-                            scale -= 5;
-                        });
-                      },
-                      side: BorderSide(color: Colors.black),
-                      activeColor: Colors.blue,
-                      // autofocus: true,
-                    ), 
-                    Text('Onions            '),
-                  ],
-                ),
-                Row(
+           Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Checkbox(
@@ -75,22 +66,127 @@ class _MyAppState extends State<MyApp> {
                         setState(() {
                           dal = !dal;
                           if (dal) {
-                            scale += 5;
+                            scale += 25;
                           } else
-                            scale -= 5;
+                            scale -= 25;
                         });
                       },
-                      side: BorderSide(color: Colors.black),
                       activeColor: Colors.blue,
-                      // autofocus: true,
                     ),
-                    Text('Chicken          '),
+                    Text(
+                      'Burger        ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                      ),
+                    ),
+                Image.asset('images/burger.png',scale:9.8 ,),
+                  ],
+                ),Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      value: cal,
+                      onChanged: (value) {
+                        setState(() {
+                          cal = !cal;
+                          if (cal) {
+                            scale += 30;
+                          } else
+                            scale -= 30;
+                        });
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                    Text(
+                      'Palov                   ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                      ),
+                    ),
+                Image.asset('images/palov.png',scale: 4,),
+                  ],
+                ),Text('Drinks',style: TextStyle(fontSize: 35,color: Colors.blue),), Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      value: sal,
+                      onChanged: (value) {
+                        setState(() {
+                          sal = !sal;
+                          if (sal) {
+                            scale += 10;
+                          } else
+                            scale -= 10;
+                        });
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                    Text(
+                      'Pepsi                           ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                      ),
+                    ),
+                Image.asset('images/pepsi.png',scale: 14,),
+                  ],
+                ),Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      value: bal,
+                      onChanged: (value) {
+                        setState(() {
+                          bal = !bal;
+                          if (bal) {
+                            scale += 11;
+                          } else
+                            scale -= 11;
+                        });
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                    Text(
+                      'Fanta                           ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                      ),
+                    ),
+                Image.asset('images/fanta.png',scale: 3,),
+                  ],
+                ),Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Checkbox(
+                      value: den,
+                      onChanged: (value) {
+                        setState(() {
+                          den = !den;
+                          if (den) {
+                            scale += 12;
+                          } else
+                            scale -= 12;
+                        });
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                    Text(
+                      'Coca-Cola               ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 20,
+                      ),
+                    ),
+                Image.asset('images/cola.png',scale: 2.7,),
                   ],
                 ),
                 Text(
                   'Total sum:  $scale so\'m',
                   style: TextStyle(
-                      fontWeight: FontWeight.w900, fontSize: 20, height: 6),
+                      fontWeight: FontWeight.w800, fontSize: 30, height: 6),
                 )
               ],
             ))),
---
+++ lib/pizza.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+// import 'package:repeat/pizza.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// bool val = false;
+// bool cal = false;
+// int ss = 0;
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Center(child: Text('Pizza menu')),
+//         ),
+//         body: Column(
+//           children: [
+//             Row(
+//               children: [
+//                 Checkbox(
+//                     value: val,
+//                     onChanged: (a) {
+//                       val = !val;
+//                       setState(() {
+//                         ss++;
+//                       });
+//                     }),
+//                 Text('Somsa'),
+//               ],
+//             ),
+//             Row(
+//               children: [
+//                 Checkbox(
+//                     value: cal,
+//                     onChanged: (a) {
+//                       cal = !cal;
+//                       setState(() {
+//                         ss++;
+//                       });
+//                     }),
+//                 Text('Manti'),
+//               ],
+//             ),
+//             Text('click$ss')
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
